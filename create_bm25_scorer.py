53ed245ff146e968c504ae3eeaa65b91457907f1
witchapong <wdaroontham@outlook.com>
2020-04-08 19:45:43 +0700

    upload to S3 after generate file in local

--- create_bm25_scorer.py
+++ create_bm25_scorer.py
@@ -10,6 +10,26 @@ from models.phrase import PhraseModel
 from pythainlp.tokenize import word_tokenize
 from gensim.summarization.bm25 import BM25
 import pickle
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+def upload_to_aws(local_file, bucket, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
 
 def create_scorer():
     from app import create_app
@@ -31,6 +51,8 @@ def create_scorer():
     pickle.dump(itoid, open('itoid.pkl','wb'))
     print('cwd:',os.getcwd())
     print('files in dir:',os.listdir())
+    print('uploading to S3...')
+    upload_to_aws(local_file='bm25_scorer.pkl', bucket=os.environ.get('S3_BUCKET_NAME'), s3_file='bm25_scorer.pkl')
 
 if __name__ == '__main__':
     create_scorer()
